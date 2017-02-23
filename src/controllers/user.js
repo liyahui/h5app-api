a6031143df0e1d07ccc8f9c89231ecf7d53ba037
liyahui <liyahui.cn@gmail.com>
2017-02-23 12:31:32 +0800

    babel只使用commonjs转换插件

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,7 @@
 import models from '../models'
 
-export const index = async (req, res) => {
-	let result = await models.User.findOne()
-	res.json(result.dataValues)
+export const index = (req, res) => {
+	models.User.findOne().then(result => {
+    res.json(result.dataValues)
+  })
 }
